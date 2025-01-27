48db536a9851fd6737f62e89efe37f87d98581dc
akshita2016 <akshita234.2016@gmail.com>
2025-01-27 20:28:35 +0530

    Solution to Input and Print a String

--- Input and Print a String@@6788d5012db25efec946f93c/code.c
+++ Input and Print a String@@6788d5012db25efec946f93c/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     char A[100];
-    scanf("%c",&A);
-    printf("You entered: %c",A);
+    scanf("%c",&A[]);
+    printf("You entered: %c",A[]);
     return 0;
 }
